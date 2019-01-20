dba03b3f1fd8d08dcea8a4f7fac727ff9089e6df
Jaeil Lee <dev@jaeil.wiki>
2019-01-21 05:46:43 +0900

    fix mis-positioned comment.

--- examples/send_sms.py
+++ examples/send_sms.py
@@ -1,8 +1,8 @@
 from PyToastSMS import ToastSMSManager
 
 msg = dict(
-    _from="01000000000",
-    _to="01000000000",  # 발신 번호는 미리 ToastConsole 에서 발신번호로 등록해 놓아야합니다.
+    _from="01000000000",  # 발신 번호는 미리 ToastConsole 에서 발신번호로 등록해 놓아야합니다.
+    _to="01000000000",
     _text="Python Toast SMS Test Message."
 )
 
